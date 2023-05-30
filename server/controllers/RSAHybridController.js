a31b89dcc83429065e57735031a601d9e9d22124
JJayJohnny <jasiek.barczewski@gmail.com>
2023-05-30 21:21:56 +0200

    added RSAHybridWorker

--- server/controllers/RSAHybridController.js
+++ server/controllers/RSAHybridController.js
@@ -1,64 +1,24 @@
-const crypto = require('crypto');
-const aesjs = require('aes-js');
-const pbkdf2 = require('pbkdf2');
+const { Worker } = require('worker_threads');
 
-module.exports.encrypt = (req, res) => {
-  console.log(Buffer.byteLength(req.files[0].buffer));
-  let keyStart = process.hrtime();
-  let symKey = pbkdf2.pbkdf2Sync(
-    process.env.password.toString(),
-    crypto.randomBytes(16),
-    1,
-    256 / 8,
-    'sha512'
-  );
-  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: Math.max(
-      (Buffer.byteLength(Buffer.from(symKey)) + 42) * 8,
-      2048
-    ),
-  });
-  let keyStop = process.hrtime(keyStart);
-  console.log(
-    'Key generation time: ' + (keyStop[0] * 1000 + keyStop[1] / 1000000) + ' ms'
-  );
+module.exports.encrypt = async (req, res) => {
+  let result = await new Promise((resolve, reject) => {
+    const worker = new Worker('./workers/RSAHybridWorker.js', {
+      workerData: {
+        file: req.files[0].buffer,
+      }
+    })
 
-  let aesEnc = new aesjs.ModeOfOperation.ctr(symKey);
-
-  let encryptStart = process.hrtime();
-
-  let encryptedData = aesEnc.encrypt(req.files[0].buffer);
-  let encryptedKey = crypto.publicEncrypt(publicKey, Buffer.from(symKey));
-
-  let encryptStop = process.hrtime(encryptStart);
-  console.log(
-    'Encryption time: ' +
-      (encryptStop[0] * 1000 + encryptStop[1] / 1000000) +
-      ' ms'
-  );
-
-  let decryptStart = process.hrtime();
-
-  let decryptedKey = crypto.privateDecrypt(privateKey, encryptedKey);
-  let aesDec = new aesjs.ModeOfOperation.ctr(decryptedKey);
-  let decryptedData = Buffer.from(aesDec.decrypt(encryptedData));
-
-  let decryptStop = process.hrtime(decryptStart);
-  console.log(
-    'Decryption time: ' +
-      (decryptStop[0] * 1000 + decryptStop[1] / 1000000) +
-      ' ms'
-  );
-
-  console.log(req.files[0].buffer);
-  console.log(decryptedData);
+    worker.once('message', (data) => {
+      resolve(data);
+    })
+  })
 
   res.send({
     alg: 'RSAHybrid',
-    encTime: encryptStop[0] * 1000 + encryptStop[1] / 1000000,
-    decTime: decryptStop[0] * 1000 + decryptStop[1] / 1000000,
-    keyTime: keyStop[0] * 1000 + keyStop[1] / 1000000,
-    totalTime: (keyStop[0] + encryptStop[0] + decryptStop[0]) * 1000 + (keyStop[1] + encryptStop[1] + decryptStop[1]) / 1000000,
+    encTime: result.encTime,
+    decTime: result.decTime,
+    keyTime: result.keyTime,
+    totalTime: result.totalTime,
     fileSize: Buffer.byteLength(req.files[0].buffer),
   })
 };
---
+++ server/workers/RSAHybridWorker.js
@@ -0,0 +1,67 @@
+const { workerData, parentPort } = require('worker_threads');
+const crypto = require('crypto');
+const aesjs = require('aes-js');
+const pbkdf2 = require('pbkdf2');
+
+function RSAHybrid(file){
+    console.log(Buffer.byteLength(file));
+  let keyStart = process.hrtime();
+  let symKey = pbkdf2.pbkdf2Sync(
+    process.env.password.toString(),
+    crypto.randomBytes(16),
+    1,
+    256 / 8,
+    'sha512'
+  );
+  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: Math.max(
+      (Buffer.byteLength(Buffer.from(symKey)) + 42) * 8,
+      2048
+    ),
+  });
+  let keyStop = process.hrtime(keyStart);
+  console.log(
+    'Key generation time: ' + (keyStop[0] * 1000 + keyStop[1] / 1000000) + ' ms'
+  );
+
+  let aesEnc = new aesjs.ModeOfOperation.ctr(symKey);
+
+  let encryptStart = process.hrtime();
+
+  let encryptedData = aesEnc.encrypt(file);
+  let encryptedKey = crypto.publicEncrypt(publicKey, Buffer.from(symKey));
+
+  let encryptStop = process.hrtime(encryptStart);
+  console.log(
+    'Encryption time: ' +
+      (encryptStop[0] * 1000 + encryptStop[1] / 1000000) +
+      ' ms'
+  );
+
+  let decryptStart = process.hrtime();
+
+  let decryptedKey = crypto.privateDecrypt(privateKey, encryptedKey);
+  let aesDec = new aesjs.ModeOfOperation.ctr(decryptedKey);
+  let decryptedData = Buffer.from(aesDec.decrypt(encryptedData));
+
+  let decryptStop = process.hrtime(decryptStart);
+  console.log(
+    'Decryption time: ' +
+      (decryptStop[0] * 1000 + decryptStop[1] / 1000000) +
+      ' ms'
+  );
+
+  console.log(Buffer.from(file));
+  console.log(decryptedData);
+
+  return{
+    encTime: encryptStop[0] * 1000 + encryptStop[1] / 1000000,
+    decTime: decryptStop[0] * 1000 + decryptStop[1] / 1000000,
+    keyTime: keyStop[0] * 1000 + keyStop[1] / 1000000,
+    totalTime: (keyStop[0] + encryptStop[0] + decryptStop[0]) * 1000 + (keyStop[1] + encryptStop[1] + decryptStop[1]) / 1000000,
+  }
+}
+
+const result = RSAHybrid(workerData.file);
+
+parentPort.postMessage(result)
